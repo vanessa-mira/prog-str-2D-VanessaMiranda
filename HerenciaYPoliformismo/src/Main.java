6a4aacaad11db4230f6057ac0c142cf62c39d5ce
vanessa-mira <20253ds117@utez.edu.mx>
2026-02-13 08:17:04 -0600

    Clase de herencia y poliformismo

---
+++ HerenciaYPoliformismo/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+//Herencia
+
+
+        Perro dogo = new Perro("Dogo");
+        Gato rawrl = new Gato("rawrl");
+
+        dogo.comer();
+        dogo.hacerSonido();
+
+        rawrl.comer();
+        rawrl.hacerSonido();
+
+        Animal[] animales =new Animal[3];
+        animales[0]=new Perro( "Ramonchis");
+        animales[1]=new Gato("Salem");
+        animales[2]=new Perro("Firulais");
+        for(Animal animal : animales){
+            animal.hacerSonido();
+        }
+        System.out.println("_________________");
+        Animal miercoles = new Perro( "miercoles");
+
+        if(miercoles instanceof Perro){
+            Perro miercoles1 = (Perro) miercoles;
+            miercoles1.marcarTerritorio();
+        }
+
+
+    }
+}
